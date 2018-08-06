ad0c4bec3d8aee3be054c3971bfc47c563a65705
Jeremy Johnson <webmaster@cloudversify.com>
2018-08-06 15:36:01 -0400

    Adding new classes to the contract for online meetings

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -96,3 +96,57 @@ contract Campaign {
         return requests.length;
     }
 }
+
+contract MeetingContract {
+    struct Meeting {
+        string title;           // title of meeting
+        string description;     // description of meeting
+        string start;           // start day and time
+        string end;             // end day and time
+        string status;          // current status (pending, active, ended)
+        address host;           // host ether address
+        address server;         // server ether address
+        uint8 quality;          // 180p, 240p, 360p, 480p, 720p, 960p, 1080p, 2160p
+        uint8 maxParticipants;  // maximum number of participants
+        bool successful;        // did meeting fail or was it successful
+        string url;             // url of the host - could use ENS name service
+        bool invitationOnly;    // are only users with ethereum addresses - and were invited - are allowed access
+        string hashedPassword;  // if meeting is authenticated, need password, 
+        // use keccak256 to make sure password matches what was hashed off-chain
+        mapping(address => bool) active; // is the user active in the meeting
+        mapping(address => bool) participant; // is this address a participant
+    }
+
+    struct Server {
+        address recipient;
+        uint maxConnections;    // rough estimate of maximum number of meeting connections a server can provide
+        bool available;         // probably just calculate this if active meetings.maxParticipants < maxConnections
+        string url;             // url where meetings will he held - could be ip address and port
+        uint8[] ports;          // possibly just port range
+    }
+
+    struct Client {
+        address clientAddress;  // ethereum client address (possibly optional)
+        string name;            // whatever name a client wants to use to display
+    }
+
+    Meeting[] meetings;
+    Client[] clients;
+    Server[] servers;
+
+    // createMeeting
+    // startMeeting
+    // stopMeeting
+    // pauseMeeting
+    // joinMeeting
+    // checkPassword
+    // reviewMeeting - Uber-style star rating
+
+    // estimateMaxLength - estimates maximum length of a meeting based on funds in user wallet
+    // payDownPayment - pay downpayment for 50% of meeting fee
+    // payServer - pay server fee for hosting meeting at end of meeting
+    // refundHost - refund if host does not run meeting, or meeting ended < 90% complete
+    // split difference between contract and server if meeting takes less than scheduled time 
+    // & is less than 10% of scheduled time - otherwise refund host meeting fee minus cost to schedule / refund
+
+}
